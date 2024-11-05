690c87187c792f189ae7e706823bcaf9f7d91749
Diego Souza <diegoshowza93@gmail.com>
2024-11-05 20:27:50 -0300

    Initial commit with model User

---
+++ db/migrate/20241105231635_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.string :name, null: false
+      t.string :email, null: false, unique: true
+      t.date :birth_date
+      t.text :notes
+      t.string :secondary_email
+      t.string :phone
+      t.string :occupation
+      t.string :emergency_contact
+
+      t.datetime :deleted_at
+      t.timestamps
+
+      t.index([ :id, :deleted_at ], unique: true)
+    end
+  end
+end
