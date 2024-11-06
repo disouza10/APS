b1e8cd2eea424ce67dc86b25651eb04e22828633
Diego Souza <diegoshowza93@gmail.com>
2024-11-05 22:37:23 -0300

    generate devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   acts_as_paranoid
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- db/migrate/20241105231635_create_users.rb
+++ db/migrate/20241105231635_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users, id: :uuid do |t|
       t.string :name, null: false
-      t.string :email, null: false
       t.date :birth_date, null: false
       t.integer :cpf, null: false
       t.text :notes
