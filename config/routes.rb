9764f68bc37281c41af68a139dda83d4718e8e39
Diego Souza <diegoshowza93@gmail.com>
2024-11-12 23:34:50 -0300

    add devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  devise_for :users
 
-  resources :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+
+  # resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
