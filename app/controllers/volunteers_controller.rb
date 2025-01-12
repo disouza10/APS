29d4b2ca0d6ab5fe7afb1d4d81859e0bbd2bd280
Diego Souza <diegoshowza93@gmail.com>
2025-01-12 18:57:56 -0300

    Update volunteer form style and set actions in controller

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,5 +1,5 @@
 class VolunteersController < ApplicationController
-  before_action :set_volunteer, only: %i[show edit destroy]
+  before_action :set_volunteer, only: %i[show edit destroy update]
 
   def index
     @volunteers = Volunteer.all
@@ -9,20 +9,52 @@ class VolunteersController < ApplicationController
   end
 
   def create
+    @volunteer = Volunteer.new(volunteer_params)
+
+    if @volunteer.save
+      redirect_to @volunteer
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @volunteer.update(volunteer_params)
+      redirect_to @volunteer
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @volunteer.destroy
+    redirect_to volunteers_path
   end
 
   private
 
   def set_volunteer
     @volunteer = Volunteer.find(params["id"])
+  end
+
+  def volunteer_params
+    params.require(:volunteer).permit(
+      :full_name,
+      :birth_date,
+      :email,
+      :secondary_email,
+      :phone,
+      :occupation,
+      :emergency_contact_name,
+      :emergency_contact_phone,
+      :address,
+      :notes,
+      :coordination_notes,
+      :current_team_id,
+      :original_team_id
+    )
   end
 end
