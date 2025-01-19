1b13202cd92342033095681d272aba72931d6c60
Diego Souza <diegoshowza93@gmail.com>
2025-01-19 18:41:03 -0300

    Set default translation and add translations to kaminari

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Aps
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "*.{rb,yml}")]
+    I18n.available_locales = [ :en, :'pt-BR' ]
+    config.i18n.default_locale = :'pt-BR'
   end
 end
